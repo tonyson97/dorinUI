2b2f1bf48f869e08e2406cf5e245b73b6d68d341
tonyson97 <notsimple99@gmail.com>
2021-10-31 10:53:53 +0700

    Updates

---
+++ precache-manifest.ad3de7dfa90ca4c173905fb26f28bc7d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "0929163d4999bae6c5b8",
+    "url": "/tonyson97/dorinUI/static/js/main.0929163d.chunk.js"
+  },
+  {
+    "revision": "dc77a65fb024aa5f5e45",
+    "url": "/tonyson97/dorinUI/static/js/1.dc77a65f.chunk.js"
+  },
+  {
+    "revision": "74850460dda22c281a27",
+    "url": "/tonyson97/dorinUI/static/js/runtime~main.74850460.js"
+  },
+  {
+    "revision": "8af824d309d9446fdafe02f2d9f610ff",
+    "url": "/tonyson97/dorinUI/index.html"
+  }
+];
